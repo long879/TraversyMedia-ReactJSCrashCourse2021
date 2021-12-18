ab9544c240737c06fadde8d0bffd90e24c76c104
Trần Thành Long <ttlong21022000@gmail.com>
2021-12-18 17:49:05 +0700

    Done task tracker traversy media youtuber

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const name = "React";
   const check = true;
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updateTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder,
+    };
+
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateTask),
+    });
+  };
+
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
 
   return (
-    <div className="App">
-      {/* JSX */}
-      <h1 style={headingStyle}>Hello from {name}!</h1>
+    <Router>
+      <div className="container">
+        {/* JSX */}
+        <h1 style={headingStyle}>Hello from {name}!</h1>
 
-      {/* Expressions in JSX */}
-      <p>{check ? "Have check" : "Haven't check"}</p>
+        {/* Expressions in JSX */}
+        <p>{check ? "Have check" : "Haven't check"}</p>
 
-      {/* Creating a component */}
-      <Header />
+        {/* Creating a component */}
+        {/* <Header /> */}
 
-      {/* Props */}
-      <Header title="Task" />
-    </div>
+        {/* Props */}
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          title="Task"
+          showAdd={showAddTask}
+        />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
-const Header = (props) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
-      <h1>{props.title}</h1>
-      <button className="btn">Add</button>
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
